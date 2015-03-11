ae42cd9f934decea9ff57a12fc254143a38e9709
Andres Cavero Rodrigo <acaverorodrigo@gmail.com>
2015-03-11 19:59:48 +0100

    changes

--- src/Component/Bootstrap.php
+++ src/Component/Bootstrap.php
@@ -4,6 +4,12 @@ namespace src\Component;
 class Bootstrap{
     public function __construct(){
         echo "hello World with Bootstrap";
+        $this->execute();
+
+    }
+    public function execute(){
+    $routing = new Routing();
+        $routing->salute();
     }
 }
 
---
+++ src/Component/Routing.php
@@ -0,0 +1,15 @@
+<?php
+namespace src\Component;
+
+
+class Routing {
+
+    public function __construct(){
+        echo "Routing";
+
+    }
+
+    public function enroute($url){
+
+    }
+}
