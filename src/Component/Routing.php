9bfe89e8afe003aa84aca3dd6afb46ccf50ad816
Andres Cavero Rodrigo <acaverorodrigo@gmail.com>
2015-03-12 11:52:18 +0100

    Routing y RoutingConfiguration

--- src/Component/Routing.php
+++ src/Component/Routing.php
@@ -8,7 +8,7 @@ class Routing {
     private $routes;
 
     public function __construct(){
-        $this->routes = require('../../Config/RouteConfiguration.php');
+        $this->routes = require('app/Config/RouteConfiguration.php');
     }
 
     public function enroute($url){
