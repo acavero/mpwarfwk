4d8d6a99257e0398a123c220f8b093d6149f504c
Andres Cavero Rodrigo <acaverorodrigo@gmail.com>
2015-03-12 12:06:50 +0100

    Routing y RoutingConfiguration

--- src/Component/Bootstrap.php
+++ src/Component/Bootstrap.php
@@ -5,17 +5,14 @@ class Bootstrap{
     public function __construct(){
         echo "hello World with Bootstrap <br>";
         $this->execute();
-
     }
-    private function execute(){
+    public function execute(){
         $routing = new Routing();
         $routing->salute();
         $currentUrl = $_SERVER['REQUEST_URI'];
         $controllerCalled = $routing->enroute($currentUrl);
         var_dump("Bootstrap del FW <br>");
-        //$controller = new $controllerCalled;
-        var_dump($controllerCalled. " Controller?\n");
-        //$controller->build();
+        return $controllerCalled;
     }
 }
 
