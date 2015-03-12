53c665ba6ac82ec9985d827cf4def4642cf71ce4
Andres Cavero Rodrigo <acaverorodrigo@gmail.com>
2015-03-12 12:54:53 +0100

    Routing y RoutingConfiguration

--- src/Component/Routing.php
+++ src/Component/Routing.php
@@ -13,8 +13,8 @@ class Routing {
 
     public function enroute($url){
         $controllerToCall = array_search($url, $this->routes);
-        var_dump($controllerToCall);
-        return ($controllerToCall);
+        var_dump("enroute ctocal " .$controllerToCall);
+        return $controllerToCall;
     }
     public function salute(){
         echo "Hola soy el routing <br>";
