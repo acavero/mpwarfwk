d08a9eb7682671e89f0dc53be7d0621981624b03
Andres Cavero Rodrigo <acaverorodrigo@gmail.com>
2015-03-13 17:32:57 +0100

    Routing y RoutingConfiguration

--- src/Component/Bootstrap.php
+++ src/Component/Bootstrap.php
@@ -7,9 +7,6 @@ class Bootstrap{
     public function execute(){
         $routing = new Routing();
         $currentUrl = $_SERVER['REQUEST_URI'];
-        if ($routing->isEmptyUrl($currentUrl)){
-            die();
-        }
         $controllerCalled = $routing->enroute($currentUrl);
         new $controllerCalled();
 
